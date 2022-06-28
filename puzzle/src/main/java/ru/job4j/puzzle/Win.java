130c6deef172338da0f4bf92339a6c0b8892ac18
Nastya <anastasiaalek67@yandex.ru>
2022-06-28 16:39:33 +0300

    games_oop_javafx

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,46 @@
 package ru.job4j.puzzle;
 
 public class Win {
+    public static boolean monoHorizontal(int[][] board) {
+        boolean result = true;
+        int counter = 0;
+        for (int k = 0; k < board.length; k++) {
+            for (int i = 0; i < board.length; i++) {
+                if ((board[k][i] != 1)  && (counter < 5)) {
+                    result = false;
+                    break;
+                } else {
+                    counter++;
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+    public static boolean monoVertical(int[][] board) {
+        boolean result = true;
+        int counter = 0;
+        for (int k = 0; k < board.length; k++) {
+            for (int i = 0; i < board.length; i++) {
+                if ((board[i][k] != 1) && (counter < 5)) {
+                    result = false;
+                    break;
+                } else {
+                    counter++;
+                    result = true;
+                }
+            }
+        }
+
+        return result;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
-        return rsl;
+        boolean result = false;
+        if (monoHorizontal(board) || monoVertical(board)) {
+            result = true;
+        }
+        return result;
     }
 }
