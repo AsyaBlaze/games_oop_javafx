776bdf2463ef43fc9fbd611839c93d343c4dc18c
Nastya <anastasiaalek67@yandex.ru>
2022-06-29 12:08:37 +0300

    games_oop_javafx

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,45 +1,35 @@
 package ru.job4j.puzzle;
 
 public class Win {
-    public static boolean monoHorizontal(int[][] board) {
+    public static boolean monoHorizontal(int[][] board, int row) {
         boolean result = true;
-        int counter = 0;
-        for (int k = 0; k < board.length; k++) {
-            for (int i = 0; i < board.length; i++) {
-                if ((board[k][i] != 1)  && (counter < 5)) {
-                    result = false;
-                    break;
-                } else {
-                    counter++;
-                    result = true;
-                }
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
             }
         }
-
         return result;
     }
-    public static boolean monoVertical(int[][] board) {
+
+    public static boolean monoVertical(int[][] board, int column) {
         boolean result = true;
-        int counter = 0;
-        for (int k = 0; k < board.length; k++) {
-            for (int i = 0; i < board.length; i++) {
-                if ((board[i][k] != 1) && (counter < 5)) {
-                    result = false;
-                    break;
-                } else {
-                    counter++;
-                    result = true;
-                }
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][column] != 1) {
+                result = false;
+                break;
             }
         }
-
         return result;
     }
 
     public static boolean check(int[][] board) {
         boolean result = false;
-        if (monoHorizontal(board) || monoVertical(board)) {
-            result = true;
+        for (int i = 0; i < board.length; i++) {
+            if ((board[i][i] == 1) && (monoHorizontal(board, i) || monoVertical(board, i))) {
+                result = true;
+                break;
+            }
         }
         return result;
     }
